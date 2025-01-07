420176240a4c6b3b4d9c12b357738ef6353c51dd
DevDyna <andrearisorti2003@gmail.com>
2025-01-08 00:17:20 +0100

    Update ItemsBlocks.java

--- src/main/java/com/devdyna/btw_ores/registry/ItemsBlocks.java
+++ src/main/java/com/devdyna/btw_ores/registry/ItemsBlocks.java
@@ -4,6 +4,7 @@ import com.devdyna.btw_ores.Main;
 
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredBlock;
@@ -20,27 +21,25 @@ public class ItemsBlocks {
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(Main.MODID);
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(Main.MODID);
 
-    public static final DeferredBlock<Block> STONE_CLUSTER_BLOCK = ClusterBlock("stone");
-    public static final DeferredBlock<Block> DEEP_CLUSTER_BLOCK = ClusterBlock("deep");
-    public static final DeferredBlock<Block> NETHER_CLUSTER_BLOCK = ClusterBlock("nether");
-    public static final DeferredBlock<Block> END_CLUSTER_BLOCK = ClusterBlock("end");
-    public static final DeferredBlock<Block> NULL_CLUSTER_BLOCK = ClusterBlock("null");
-
-    
+    public static final DeferredBlock<Block> STONE_CLUSTER_BLOCK = regClusterBlock("stone");
+    public static final DeferredBlock<Block> DEEP_CLUSTER_BLOCK = regClusterBlock("deep");
+    public static final DeferredBlock<Block> NETHER_CLUSTER_BLOCK = regClusterBlock("nether");
+    public static final DeferredBlock<Block> END_CLUSTER_BLOCK = regClusterBlock("end");
+    public static final DeferredBlock<Block> NULL_CLUSTER_BLOCK = regClusterBlock("null");
 
     public static final DeferredItem<BlockItem> STONE_CLUSTER_ITEM = ClusterItem(STONE_CLUSTER_BLOCK);
-    public static final DeferredItem<BlockItem> DEEP_CLUSTER_ITEM = ClusterItem( DEEP_CLUSTER_BLOCK);
-    public static final DeferredItem<BlockItem> NETHER_CLUSTER_ITEM = ClusterItem( NETHER_CLUSTER_BLOCK);
-    public static final DeferredItem<BlockItem> END_CLUSTER_ITEM = ClusterItem( END_CLUSTER_BLOCK);
-    public static final DeferredItem<BlockItem> NULL_CLUSTER_ITEM = ClusterItem( NULL_CLUSTER_BLOCK);
+    public static final DeferredItem<BlockItem> DEEP_CLUSTER_ITEM = ClusterItem(DEEP_CLUSTER_BLOCK);
+    public static final DeferredItem<BlockItem> NETHER_CLUSTER_ITEM = ClusterItem(NETHER_CLUSTER_BLOCK);
+    public static final DeferredItem<BlockItem> END_CLUSTER_ITEM = ClusterItem(END_CLUSTER_BLOCK);
+    public static final DeferredItem<BlockItem> NULL_CLUSTER_ITEM = ClusterItem(NULL_CLUSTER_BLOCK);
 
-    public static DeferredBlock<Block> ClusterBlock(String name) {
-        return BLOCKS.registerSimpleBlock( name+"_cluster",
-                BlockBehaviour.Properties.of().destroyTime(10).explosionResistance(100));
+    public static DeferredBlock<Block> regClusterBlock(String name) {
+        return BLOCKS.register(name + "_cluster", () -> new ClusterBlock(
+                BlockBehaviour.Properties.of().randomTicks().destroyTime(100).explosionResistance(100)
+                        .sound(SoundType.ANCIENT_DEBRIS).noLootTable()));
     }
 
-
-    public static DeferredItem<BlockItem> ClusterItem(DeferredBlock<Block> block ) {
+    public static DeferredItem<BlockItem> ClusterItem(DeferredBlock<Block> block) {
         return ITEMS.registerSimpleBlockItem(block);
     }
 }
