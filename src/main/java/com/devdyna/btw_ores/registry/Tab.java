2d3e0c6ffe3985dd69b2920c2feb9891b655ca87
DevDyna <andrearisorti2003@gmail.com>
2025-01-09 16:33:49 +0100

    Update Tab.java

--- src/main/java/com/devdyna/btw_ores/registry/Tab.java
+++ src/main/java/com/devdyna/btw_ores/registry/Tab.java
@@ -1,19 +1,19 @@
 package com.devdyna.btw_ores.registry;
 
 import com.devdyna.btw_ores.Main;
+import com.devdyna.btw_ores.utils.Constants;
 
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 public class Tab {
-
-    Tab() {
-    }
 
     public static void register(IEventBus bus) {
         CREATIVE_MODE_TABS.register(bus);
@@ -22,15 +22,19 @@ public class Tab {
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister
             .create(Registries.CREATIVE_MODE_TAB, Main.MODID);
     public static final DeferredHolder<CreativeModeTab, CreativeModeTab> CLUSTER_TAB = CREATIVE_MODE_TABS
-            .register("cluster_tab", () -> CreativeModeTab.builder()
-                    .title(Component.translatable(Main.MODID + ".cluster.tab"))
+            .register(Main.MODID + "_tab", () -> CreativeModeTab.builder()
+                    .title(Component.translatable(Main.MODID + ".tab"))
                     .withTabsBefore(CreativeModeTabs.COMBAT)
-                    .icon(() -> ItemsBlocks.STONE_CLUSTER_ITEM.get().getDefaultInstance())
+                    .icon(() -> ItemsBlocks.SCANNER.get().getDefaultInstance())
                     .displayItems((parameters, output) -> {
-                        output.accept(ItemsBlocks.STONE_CLUSTER_BLOCK.get());
-                        output.accept(ItemsBlocks.DEEP_CLUSTER_BLOCK.get());
-                        output.accept(ItemsBlocks.NETHER_CLUSTER_BLOCK.get());
-                        output.accept(ItemsBlocks.END_CLUSTER_BLOCK.get());
-                        output.accept(ItemsBlocks.NULL_CLUSTER_BLOCK.get());
+
+                        for (Block block : Constants.AllBlocks) {
+                            output.accept(block);
+                        }
+
+                        for (Item item : Constants.AllItems) {
+                            output.accept(item);
+                        }
+
                     }).build());
 }
