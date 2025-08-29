fb7170ec199a3ed8873e7a6773959ed61c81d766
DevDyna <andrearisorti2003@gmail.com>
2025-08-29 16:47:33 +0200

    Update ClusterBlock.java

--- src/main/java/com/devdyna/btw_ores/registry/builders/ClusterBlock.java
+++ src/main/java/com/devdyna/btw_ores/registry/builders/ClusterBlock.java
@@ -4,28 +4,32 @@ import java.util.List;
 
 import javax.annotation.Nullable;
 
+import com.devdyna.btw_ores.Config;
 import com.devdyna.btw_ores.Main;
-import com.devdyna.btw_ores.registry.BlockTags;
+import com.devdyna.btw_ores.api.ClusterApi;
 import com.devdyna.btw_ores.registry.ItemsBlocks;
+import com.devdyna.btw_ores.registry.zTags;
 import com.devdyna.btw_ores.utils.LevelUtil;
-import com.devdyna.btw_ores.utils.Math;
+import com.mojang.logging.LogUtils;
 
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.tags.TagKey;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.EntityBlock;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraftforge.common.Tags;
 
-public class ClusterBlock extends Block {
 @SuppressWarnings("null")
 public class ClusterBlock extends Block implements EntityBlock {
 
@@ -36,13 +40,12 @@ public class ClusterBlock extends Block implements EntityBlock {
         this.type = type;
     }
 
-    @SuppressWarnings("null")
+    @SuppressWarnings({ "deprecation" })
     public void randomTick(BlockState state, ServerLevel world, BlockPos pos, RandomSource random) {
 
         Level level = world.getLevel();
-    @SuppressWarnings("deprecation")
 
-        if (LevelUtil.chance(75, level) || state.is(ItemsBlocks.NULL_CLUSTER_BLOCK))
+        if (LevelUtil.chance(75, level) || state.is(ItemsBlocks.NULL_CLUSTER_BLOCK.get()))
             return;
 
         var index = ClusterApi.getIndex(state, pos, level);
@@ -54,7 +57,7 @@ public class ClusterBlock extends Block implements EntityBlock {
             if (LevelUtil.ValidFaces(pos, level, validReGrow) >= 3) {
                 BlockState ore;
                 try {
-                    var oreID = level.getBlockEntity(pos).saveCustomAndMetadata(level.registryAccess())
+                    var oreID = level.getBlockEntity(pos).saveWithFullMetadata()
                             .getString("oreBroken").split(":");
 
                     if (Config.RANDOM_CLUSTERS.get() && blockTag != null)
@@ -63,7 +66,7 @@ public class ClusterBlock extends Block implements EntityBlock {
                                         LevelUtil.getRandomValue(LevelUtil.getSizeTag(blockTag), level))
                                 .defaultBlockState();
                     else
-                        ore = BuiltInRegistries.BLOCK.get(ResourceLocation.fromNamespaceAndPath(oreID[0], oreID[1]))
+                        ore = BuiltInRegistries.BLOCK.get(new ResourceLocation(oreID[0], oreID[1]))
                                 .defaultBlockState();
 
                     if (ore == null || ore.is(zTags.NO_CLUSTER_RESULT))
@@ -72,11 +75,12 @@ public class ClusterBlock extends Block implements EntityBlock {
                     level.setBlockAndUpdate(pos, ore);
                 } catch (Exception e1) {
                     // when nbt blockid wasn't correct
-                    LogUtils.getLogger().error("##ERROR## Found a block cluster that contain broken value at " + pos.toShortString());
+                    LogUtils.getLogger().error(
+                            "##ERROR## Found a block cluster that contain broken value at " + pos.toShortString());
                     try {
                         LogUtils.getLogger()
                                 .info("##-----## Value stored inside BE: "
-                                        + level.getBlockEntity(pos).saveCustomAndMetadata(level.registryAccess())
+                                        + level.getBlockEntity(pos).saveWithFullMetadata()
                                                 .getString("oreBroken"));
 
                     } catch (Exception e2) {
@@ -94,23 +98,20 @@ public class ClusterBlock extends Block implements EntityBlock {
                 }
             }
 
-        }
-
     }
 
-    @SuppressWarnings("null")
     @Override
     public void appendHoverText(ItemStack stack, @Nullable BlockGetter level, List<Component> context,
             TooltipFlag flag) {
         if (Screen.hasControlDown()) {
-            if (Config.RANDOM_CLUSTERS.getAsBoolean())
-                tooltipComponents.add(Component.translatable(Main.MODID + "." + type + ".on"));
+            if (Config.RANDOM_CLUSTERS.get())
+                context.add(Component.translatable(Main.MODID + "." + type + ".on"));
             else
-                tooltipComponents.add(Component.translatable(Main.MODID + ".randomless"));
+                context.add(Component.translatable(Main.MODID + ".randomless"));
         } else {
-            tooltipComponents.add(Component.translatable(Main.MODID + ".off"));
+            context.add(Component.translatable(Main.MODID + ".off"));
         }
-        super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
+        super.appendHoverText(stack, level, context, flag);
     }
 
     @Override
