2aa676e9769d32f9f888ff69f3d7d647e38133dc
DevDyna <andrearisorti2003@gmail.com>
2025-08-29 16:19:03 +0200

    Update Scanner.java

--- src/main/java/com/devdyna/btw_ores/registry/builders/Scanner.java
+++ src/main/java/com/devdyna/btw_ores/registry/builders/Scanner.java
@@ -93,7 +93,7 @@ public class Scanner extends Item {
         } else {
             context.add(Component.translatable(Main.MODID + ".off"));
         }
-        super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
+        super.appendHoverText(stack,level,context,flag);
     }
 
     private Boolean validDimension(String type, BlockPos pos, Level level) {
