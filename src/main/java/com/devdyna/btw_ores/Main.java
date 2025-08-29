794c8937ac339f680611dfb3765b407d14e09d7e
DevDyna <andrearisorti2003@gmail.com>
2025-08-29 16:47:19 +0200

    Update Main.java

--- src/main/java/com/devdyna/btw_ores/Main.java
+++ src/main/java/com/devdyna/btw_ores/Main.java
@@ -6,22 +6,21 @@ import com.devdyna.btw_ores.registry.Tab;
 
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModContainer;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.config.ModConfig;
 
 @Mod(Main.MODID)
 public class Main {
 
     public static final String MODID = "btw_ores";
 
-    public Main() {
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        // item reg
-        ItemsBlocks.register(modEventBus);
-        // events
-        MinecraftForge.EVENT_BUS.register(new BlockBreak());
-        // tab reg
+    public Main(IEventBus modEventBus, ModContainer modContainer) {
         Tab.register(modEventBus);
+        ItemsBlocks.register(modEventBus);
+        MinecraftForge.EVENT_BUS.register(new BlockBreak());
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.CONFIG_SPEC);
     }
 
 }
