54dfe5f3c9ae261f3cc87d5d94e8bbedd12aabfe
DevDyna <andrearisorti2003@gmail.com>
2025-08-29 16:47:31 +0200

    Create ClusterBE.java

---
+++ src/main/java/com/devdyna/btw_ores/registry/builders/ClusterBE.java
@@ -0,0 +1,37 @@
+package com.devdyna.btw_ores.registry.builders;
+
+import com.devdyna.btw_ores.registry.ItemsBlocks;
+import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+
+@SuppressWarnings("null")
+public class ClusterBE extends BlockEntity {
+
+    private String oreMined;
+
+    public ClusterBE(BlockEntityType<?> type, BlockPos pos, BlockState blockState) {
+        super(type, pos, blockState);
+        this.oreMined = null;
+    }
+
+    public ClusterBE(BlockPos pos, BlockState blockState) {
+        this(ItemsBlocks.CLUSTER.get(), pos, blockState);
+    }
+
+    @Override
+    protected void saveAdditional(CompoundTag tag) {
+        super.saveAdditional(tag);
+        tag.putString("oreBroken", oreMined != null ? oreMined : "");
+    }
+
+    @Override
+    public void load(CompoundTag tag) {
+        super.load(tag);
+        if (tag.contains("oreBroken"))
+            oreMined = tag.getString("oreBroken");
+    }
+
+}
