5a78ac7a80d290933123b6566e8af8e26e63484c
DevDyna <andrearisorti2003@gmail.com>
2025-08-29 16:47:41 +0200

    Update zTags.java

--- src/main/java/com/devdyna/btw_ores/registry/zTags.java
+++ src/main/java/com/devdyna/btw_ores/registry/zTags.java
@@ -12,14 +12,14 @@ public class zTags {
 
     public static final TagKey<Block> NO_CLUSTER_GEN = createBlockTag(Main.MODID, "cannot_spawn_cluster");
     public static final TagKey<Block> NO_CLUSTER_RESULT = createBlockTag(Main.MODID, "cannot_generated_by_cluster");
-    
+
     public static final TagKey<Block> ORES_IN_GROUND_END = createBlockTag("c", "ores_in_ground/end");
     public static final TagKey<Block> CLUSTERS = createBlockTag(Main.MODID, "clusters");
     public static final TagKey<Block> VALID_REGROW_STONE = createBlockTag(Main.MODID, "rocks/stone");
     public static final TagKey<Block> VALID_REGROW_DEEPSLATE = createBlockTag(Main.MODID, "rocks/deepslate");
     public static final TagKey<Block> VALID_REGROW_NETHER = createBlockTag(Main.MODID, "rocks/nether");
     public static final TagKey<Block> VALID_REGROW_END = createBlockTag(Main.MODID, "rocks/end");
-    
+
     public static final TagKey<Biome> BIOME_OVERWORLD = createBiomeTag(Main.MODID, "is_overworld");
     public static final TagKey<Biome> BIOME_UNDERGROUND = createBiomeTag(Main.MODID, "is_underground");
     public static final TagKey<Biome> BIOME_NETHER = createBiomeTag(Main.MODID, "is_nether");
@@ -31,7 +31,7 @@ public class zTags {
                 new ResourceLocation(modId, path));
     }
 
-        private static TagKey<Biome> createBiomeTag(String modId, String path) {
+    private static TagKey<Biome> createBiomeTag(String modId, String path) {
         return TagKey.create(Registries.BIOME,
                 new ResourceLocation(modId, path));
     }
